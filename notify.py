1942fd8ad9c003b223e3c43a68a46a13e59bcbcb
Major Hayden <major@mhtx.net>
2022-02-02 08:08:11 -0600

    Log the scheduled job list on startup

--- notify.py
+++ notify.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Set the schedule for running various notification tasks."""
 import logging
 import time
 
@@ -11,12 +12,18 @@ from thetagang_notifications import trends
 log = logging.getLogger(__name__)
 log.info("🚀 thetagang-notifications starting up!")
 
+
 # Set up simple functions to call the methods in each module.
 def notify_for_trends():
+    """Notify for new trends."""
     trends.main()
+
 
 # Schedule the runs of various tasks.
 schedule.every(1).minutes.do(notify_for_trends)
+
+for upcoming_job in schedule.get_jobs():
+    log.info("⏰ %s", upcoming_job.__repr__())
 
 # Run pending tasks forever.
 while True:
