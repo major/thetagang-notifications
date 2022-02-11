36185e0dd5936fced26871fc204e85e0c94a6d30
Major Hayden <major@mhtx.net>
2022-02-11 10:47:32 -0600

    Update utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,113 +1,9 @@
 """Tests for utils functions."""
-from datetime import datetime
-import json
-
-from freezegun import freeze_time
 import pytest
 import requests_mock
 
 
 from thetagang_notifications import utils
-
-
-def load_trade_asset(asset_filename):
-    """Load a trade asset from test assets."""
-    with open(f"tests/assets/{asset_filename}", "r") as fileh:
-        return json.load(fileh)
-
-
-def test_breakeven_short_put():
-    """Test short option breakevens."""
-    trade = {"type": "CASH SECURED PUT", "short_put": "100", "price_filled": 1.50}
-    assert utils.get_breakeven(trade) == 98.50
-
-
-def test_breakeven_covered_call():
-    """Test short option breakevens."""
-    trade = {"type": "COVERED CALL", "short_call": "100", "price_filled": 1.50}
-    assert utils.get_breakeven(trade) == 101.50
-
-
-def test_breakeven_short_call():
-    """Test short option breakevens."""
-    trade = {"type": "SHORT NAKED CALL", "short_call": "100", "price_filled": 1.50}
-    assert utils.get_breakeven(trade) == 101.50
-
-
-def test_breakeven_unknown():
-    """Test breakevens when we can't calculate them."""
-    trade = load_trade_asset("trade-short-iron-condor.json")
-    assert utils.get_breakeven(trade) is None
-
-
-@freeze_time("2022-02-03")
-def test_short_put_return():
-    """Verify that we calculate put breakevens correctly."""
-    trade = {
-        "short_put": 190,
-        "price_filled": 1.73,
-        "expiry_date": "2022-02-18T00:00:00.000Z",
-    }
-    result = utils.get_short_put_return(trade)
-    assert result == 0.918893079088543
-
-
-@freeze_time("2022-02-03")
-def test_short_call_return():
-    """Verify that we calculate call breakevens correctly."""
-    trade = load_trade_asset("trade-covered-call.json")
-    print(trade)
-    result = utils.get_short_call_return(trade)
-    assert result == 0.6385341476957246
-
-
-def test_gather_strikes():
-    """Ensure we can gather strikes in a generic way."""
-    trade = load_trade_asset("trade-short-iron-condor.json")
-    strikes = utils.gather_strikes(trade)
-    assert strikes == "$115/$150/$110/$155"
-
-
-def test_parse_expiry_date():
-    """Verify that JSON timestamps for expiration dates convert correctly."""
-    expiry_date = "2022-03-17T00:00:00.000Z"
-    parsed_date = datetime(2022, 3, 17)
-    assert utils.parse_expiry_date(expiry_date) == parsed_date
-
-
-def test_parse_expiry_date_missing():
-    """Handle trades without an expiration date."""
-    expiry_date = None
-    assert utils.parse_expiry_date(expiry_date) is None
-
-
-@freeze_time("2022-02-01")
-def test_get_dte():
-    """Verify that DTE is calculated correctly."""
-    expiration = "2022-03-18T00:00:00.000Z"
-    expected_dte = 45
-    assert utils.get_dte(expiration) == expected_dte
-
-
-@freeze_time("2022-02-01")
-def test_get_pretty_expiration():
-    """Verify that we generate expiration dates properly in a pretty way."""
-    # < 1 year away.
-    expiry = "2022-03-17T00:00:00.000Z"
-    assert utils.get_pretty_expiration(expiry) == "03/17"
-
-    # > 1 year away.
-    expiry = "2023-02-02T00:00:00.000Z"
-    assert utils.get_pretty_expiration(expiry) == "02/02/23"
-
-    # Looking backwards.
-    expiry = "2021-02-02T00:00:00.000Z"
-    assert utils.get_pretty_expiration(expiry) == "02/02"
-
-
-def test_get_pretty_expiration_no_date():
-    """Test a pretty expiration parse with no expiration date (buying stocks)."""
-    assert utils.get_pretty_expiration(None) is None
 
 
 def test_get_stock_chart():
@@ -117,31 +13,19 @@ def test_get_stock_chart():
     assert chart_url == expected_url
 
 
-def test_get_logo():
-    """Ensure we generate a stock logo properly."""
-    assert utils.get_logo("AMD").endswith("AMD.png")
-    assert utils.get_logo("sPy").endswith("SPY.png")
-
-
-def test_get_finviz_stock_equity(mocker):
+@pytest.mark.vcr()
+def test_get_finviz_stock_equity():
     """Ensure we get stock data from finviz properly."""
-    mocker.patch(
-        target="thetagang_notifications.utils.finviz.get_stock",
-        return_value=load_trade_asset("finviz-amd.json"),
-    )
     result = utils.get_finviz_stock("AMD")
     assert result["Website"] == "https://www.amd.com"
     assert result["Sector"] == "Technology"
 
 
-def test_get_finviz_stock_etf(mocker):
+@pytest.mark.vcr()
+def test_get_finviz_stock_etf():
     """Ensure we get stock data from finviz properly."""
-    mocker.patch(
-        target="thetagang_notifications.utils.finviz.get_stock",
-        return_value=load_trade_asset("finviz-soxl.json"),
-    )
-    result = utils.get_finviz_stock("AMD")
-    assert result["Website"] is None
+    result = utils.get_finviz_stock("SOXL")
+    assert not result["Website"]
     assert result["Sector"] == "Exchange Traded Fund"
 
 
@@ -167,9 +51,6 @@ def test_get_stock_logo_success(requests_mock, mocker):
         target="thetagang_notifications.utils.get_logo_clearbit", return_value=None
     )
     result = utils.get_stock_logo("AMD")
-    from pprint import pprint
-
-    pprint(result)
     assert result == secondary_url
 
 
@@ -209,38 +90,14 @@ def test_get_logo_iex_placeholder():
         assert result is None
 
 
+@pytest.mark.vcr()
 def test_get_logo_clearbit(mocker):
     """Ensure we get a logo from clearbit."""
-    # Try it with a complete URL first.
-    amd_data = load_trade_asset("finviz-amd.json")
-    mocker.patch(
-        target="thetagang_notifications.utils.finviz.get_stock",
-        return_value=amd_data,
-    )
     result = utils.get_logo_clearbit("AMD")
     assert result == "https://logo.clearbit.com/amd.com"
 
-    # Now try it without a URL.
-    soxl_data = load_trade_asset("finviz-soxl.json")
-    mocker.patch(
-        target="thetagang_notifications.utils.finviz.get_stock",
-        return_value=soxl_data,
-    )
     result = utils.get_logo_clearbit("SOXL")
     assert result is None
 
-
-def test_get_symbol_details(mocker):
-    """Verify getting stock data."""
-    mocked_class = mocker.patch(target="thetagang_notifications.utils.yf.Ticker")
-    utils.get_symbol_details("AMD")
-    mocked_class.assert_called_with("AMD")
-
-    mocked_class = mocker.patch(
-        target="thetagang_notifications.utils.yf.Ticker",
-        side_effect=Exception("something broke"),
-    )
-    with pytest.raises(Exception) as excinfo:
-        utils.get_symbol_details("AMD")
-        mocked_class.assert_called_with("AMD")
-        assert excinfo.value.message == "something broke"
+    result = utils.get_logo_clearbit("DOOTS")
+    assert result is None
--- thetagang_notifications/utils.py
+++ thetagang_notifications/utils.py
@@ -1,12 +1,9 @@
 """Small utilities for small tasks."""
-from datetime import datetime
 import logging
 
-from dateutil import parser
 import finviz
 import requests
 import tld
-import yfinance as yf
 
 
 log = logging.getLogger(__name__)
@@ -20,111 +17,6 @@ IEX_SECONDARY_LOGO_URL = "https://storage.googleapis.com/iex/api/logos"
 # image with an ugly, wavy picture. The picture has a common MD5 hash that we can
 # detect and work around.
 IEX_PLACEHOLDER_IMAGE_HASH = "md5=ZLE6FlAxyV8t6arLO5AFeg=="
-
-
-def get_breakeven(trade):
-    """Return the breakeven on a cash secured or naked put."""
-    premium = float(trade["price_filled"])
-    match trade["type"]:
-        case "CASH SECURED PUT":
-            strike = float(trade["short_put"])
-            result = strike - premium
-        case "COVERED CALL" | "SHORT NAKED CALL":
-            strike = float(trade["short_call"])
-            result = strike + premium
-        case _:
-            return None
-
-    return result
-
-
-def get_short_put_return(trade):
-    """Calculate the return and annualized return for a short put."""
-    strike = float(trade["short_put"])
-    premium = trade["price_filled"]
-
-    return (premium / (strike - premium)) * 100
-
-
-def get_short_call_return(trade):
-    """Calculate the return and annualized return for a short call."""
-    strike = float(trade["short_call"])
-    premium = trade["price_filled"]
-
-    return (premium / (strike - premium)) * 100
-
-
-def get_annualized_return(potential_return, dte):
-    """Calculate the annualized return for a trade."""
-    if dte == 0:
-        # Computers dislike dividing by zero. It hurts.
-        dte = 1
-    elif dte < 0:
-        # We aren't time travelers here.
-        return None
-    return (potential_return / dte) * 365
-
-
-def parse_expiry_date(expiry_date):
-    """Convert JSON expiration date into Python date object"""
-    if not expiry_date:
-        return None
-
-    return parser.parse(expiry_date, ignoretz=True)
-
-
-def gather_strikes(trade):
-    """Gather option strikes in a generic way."""
-    strikes = {
-        "short put": trade["short_put"],
-        "short call": trade["short_call"],
-        "long put": trade["long_put"],
-        "long call": trade["long_call"],
-    }
-
-    # Make a string from the generic list of strikes.
-    return "/".join([f"${v}" for k, v in strikes.items() if v is not None]).capitalize()
-
-
-def get_dte(expiry_date):
-    """Calculate days to expiry (DTE) for a trade."""
-    parsed_expiry = parse_expiry_date(expiry_date)
-    return (parsed_expiry - datetime.now()).days
-
-
-def get_pretty_expiration(expiry_date):
-    """Generate a pretty expiration date for trade notifications."""
-    if not expiry_date:
-        return None
-
-    parsed_date = parse_expiry_date(expiry_date)
-    dte = get_dte(expiry_date)
-    if dte <= 365:
-        return parsed_date.strftime("%m/%d")
-
-    return parsed_date.strftime("%m/%d/%y")
-
-
-def get_pretty_price(price):
-    """Generate a pretty price based on a float."""
-    return "{:,.2f}".format(price)
-
-
-def get_symbol_details(symbol):
-    """Get information about a stock/ETF symbol."""
-    try:
-        stock = yf.Ticker(symbol)
-        details = stock.get_info()
-    except:
-        details = {"symbol": symbol}
-
-    return details
-
-
-def get_logo(symbol):
-    """Generate a URL to a stock logo on IEX cloud."""
-    # More details: https://iexcloud.io/docs/api/#logo
-    return f"https://storage.googleapis.com/iex/api/logos/{symbol.upper()}.png"
 
 
 def get_stock_logo(symbol):
@@ -181,7 +73,7 @@ def get_logo_clearbit(symbol):
         return None
 
     # Ensure the finviz result has a URL listed.
-    if "Website" not in finviz_data.keys() or not finviz_data["Website"]:
+    if not finviz_data["Website"]:
         return None
 
     domain = tld.get_fld(finviz_data["Website"])
