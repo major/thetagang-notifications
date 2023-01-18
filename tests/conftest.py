02f2cba6c11e77d08fb6c5c4173047e72492729a
Major Hayden 🤠 <major@mhtx.net>
2023-01-18 15:26:54 -0600

    Use a parameterized test fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,32 +3,27 @@ import pytest
 import requests
 import vcr
 
+# List of trades on thetagang.com to use as data for tests.
 REAL_TRADES = {
-    "CASH_SECURED_PUT": "c90e5d5d-8158-43d7-ba09-e6a0dbbf207c",
-    "COVERED_CALL": "2093163e-2d7b-424f-b007-51c46ace7bb4",
-    "PUT_CREDIT_SPREAD": "7a9fe9d3-b1aa-483c-bcc0-b6f73c6b4eec",
-    "SHORT_IRON_CONDOR": "8cdf95fd-d0d4-4f47-ae81-a1e000979291",
-    "BUY_COMMON_STOCK": "72060eaa-7803-4124-be5b-c03b54171e75",
-    "SELL_COMMON_STOCK": "a2a46a7f-1c7c-4328-98d7-32b900af98c1",
-    "LONG_NAKED_PUT": "19a3beee-95e9-4c19-ab1d-712793f3eeaf",
-    "SHORT_NAKED_CALL": "665b5d15-7009-49b7-8746-0e7effdf0829",
-    "NON_PATRON_TRADE": "36ad8e04-b2a1-40c8-8632-55e491be10ca",
+    "CASH SECURED PUT": "c90e5d5d-8158-43d7-ba09-e6a0dbbf207c",
+    "COVERED CALL": "2093163e-2d7b-424f-b007-51c46ace7bb4",
+    "PUT CREDIT SPREAD": "7a9fe9d3-b1aa-483c-bcc0-b6f73c6b4eec",
+    "SHORT IRON CONDOR": "8cdf95fd-d0d4-4f47-ae81-a1e000979291",
+    "BUY COMMON STOCK": "72060eaa-7803-4124-be5b-c03b54171e75",
+    "SELL COMMON STOCK": "a2a46a7f-1c7c-4328-98d7-32b900af98c1",
+    "LONG NAKED PUT": "19a3beee-95e9-4c19-ab1d-712793f3eeaf",
+    "SHORT NAKED CALL": "665b5d15-7009-49b7-8746-0e7effdf0829",
 }
 
 
-def real_trade_downloader(trade_type, trade_guid):
+@pytest.fixture(scope="session")
+def real_trade(request):
     """Get a real trade from thetagang.com."""
-    with vcr.use_cassette(f"tests/fixtures/trades/{trade_type}.yaml"):
-        url = f"https://api.thetagang.com/trades/{trade_guid}"
-        print(url)
+    if request.param not in REAL_TRADES:
+        raise ValueError(f"No real trade guid fixture for `{request.param}`.")
+
+    with vcr.use_cassette(f"tests/fixtures/trades/{request.param}.yaml"):
+        url = f"https://api.thetagang.com/trades/{REAL_TRADES[request.param]}"
         trade = requests.get(url).json()["data"]["trade"]
 
     return trade
-
-
-@pytest.fixture(scope="session")
-def cash_secured_put():
-    """Get a cash secured put trade."""
-    return real_trade_downloader(
-        "cash_secured_put", "c90e5d5d-8158-43d7-ba09-e6a0dbbf207c"
-    )
--- tests/test_notification.py
+++ tests/test_notification.py
@@ -1,26 +1,15 @@
 """Test discord notifications for trades."""
-from thetagang_notifications.config import CLOSING_TRADE_ICON, OPENING_TRADE_ICON
-from thetagang_notifications.notification import (
-    ClosingNotification,
-    OpeningNotification,
-)
+import pytest
+
+from thetagang_notifications.config import CLOSING_TRADE_ICON
+from thetagang_notifications.notification import ClosingNotification
 from thetagang_notifications.trades import Trade
 
 
-def test_opening_notification(cash_secured_put):
-    """Test opening notification."""
-    trade = Trade(cash_secured_put)
-    notification = OpeningNotification(trade)
-    assert notification.action_line() == {
-        "name": f"{trade.username} opened a trade",
-        "url": trade.trade_url,
-        "icon_url": OPENING_TRADE_ICON,
-    }
-
-
-def test_closing_notification(cash_secured_put):
+@pytest.mark.parametrize("real_trade", ["CASH SECURED PUT"], indirect=True)
+def test_closing_notification(real_trade):
     """Test closing notification."""
-    trade = Trade(cash_secured_put)
+    trade = Trade(real_trade)
     notification = ClosingNotification(trade)
     assert notification.action_line() == {
         "name": f"{trade.username} closed a trade",
