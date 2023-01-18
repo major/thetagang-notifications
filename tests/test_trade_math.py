d6ebcaa92d245cd1b1bc3d11282b0841cc0f9afb
Major Hayden 🤠 <major@mhtx.net>
2023-01-18 17:26:35 -0600

    Improve trade_math testing

--- tests/test_trade_math.py
+++ tests/test_trade_math.py
@@ -20,3 +20,15 @@ def test_breakeven_other():
     """Ensure the breakeven function handles other trades."""
     trade = {"type": "SHORT IRON CONDOR"}
     assert trade_math.breakeven(trade) is None
+
+
+def test_short_call_breakeven():
+    """Verify the breakeven on a short call."""
+    breakeven = trade_math.short_call_breakeven("100", 1.00)
+    assert breakeven == "101.00"
+
+
+def tests_short_put_breakeven():
+    """Verify the breakeven on a short put."""
+    breakeven = trade_math.short_put_breakeven("100", 1.00)
+    assert breakeven == "99.00"
