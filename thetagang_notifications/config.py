04a80c876e6e2644fd88c67e08458e43492f4366
Major Hayden <major@mhtx.net>
2024-03-05 12:57:44 -0600

    config.py: Type fixes

--- thetagang_notifications/config.py
+++ thetagang_notifications/config.py
@@ -4,7 +4,7 @@ import os
 
 # Redis connection details
 REDIS_HOST: str = os.environ.get("REDIS_HOST", "localhost")
-REDIS_PORT: int = os.environ.get("REDIS_PORT", 6379)
+REDIS_PORT: int = int(os.environ.get("REDIS_PORT", 6379))
 
 # thetagang.com URLs
 TRADES_JSON_URL = "https://api3.thetagang.com/trades"
