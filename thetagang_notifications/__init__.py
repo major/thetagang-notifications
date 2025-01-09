18d5d1c216c469d9a7f081215dbd10709f39141f
Major Hayden <major@mhtx.net>
2025-01-09 08:45:46 -0600

    Update to poetry 2.0

--- thetagang_notifications/__init__.py
+++ thetagang_notifications/__init__.py
@@ -3,8 +3,6 @@
 import logging
 import sys
 
-import sentry_sdk
-
 logging.basicConfig(
     stream=sys.stdout,
     level=logging.INFO,
@@ -14,8 +12,3 @@ logging.basicConfig(
 __author__ = """Major Hayden"""
 __email__ = "major@mhtx.net"
 __version__ = "0.1.0"
-
-sentry_sdk.init(
-    dsn="https://c62e0d37db5d46b991a641b35878042a@o4504556291358720.ingest.sentry.io/4504556292341761",
-    traces_sample_rate=1.0,
-)
